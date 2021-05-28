3284c3be9e7327f231f0ff5f6e795ea1846681c2
John Adewumi <johnadewumi63@gmail.com>
2021-05-28 14:02:07 -0700

    neww

--- provider.tf
+++ provider.tf
@@ -3,7 +3,8 @@ provider "oci" {
   region = "us-sanjose-1"
   user_ocid = "ocid1.user.oc1..aaaaaaaamrhp2f3m2evpmlme32kqavvgynxaz66oxfvrdephahsf72mwk6cq"
   fingerprint = "39:00:25:bf:cf:9b:00:70:87:f5:75:74:7b:71:b1:40"
-  private_key_path = "/home/opc/.oci/oci_api_key.pem"
+  private_key_path = "/home/opc/credentials/oci_api_key.pem"
   private_key_oci = "/home/opc/credentials/id_rsa"
   public_key_oci = "/home/opc/credentials/id_rsa.pub"
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaapqytcu462c27feapv4bvf2ijszoqm7qmqjn4mx3koz3o5tjt5ska"
 }
