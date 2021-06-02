8c7e98d95c48f6dba555ee28f60472097cd59844
John Adewumi <johnadewumi63@gmail.com>
2021-06-02 10:50:30 -0700

    newest

--- data.tf
+++ data.tf
@@ -5,3 +5,15 @@ data "template_file" "ad_names" {
   count    = length(data.oci_identity_availability_domains.ad.availability_domains)
   template = lookup(data.oci_identity_availability_domains.ad.availability_domains[count.index], "name")
 }
+
+
+data "oci_core_images" "tf_images" {
+  compartment_id           = var.compartment_id
+  shape                    = var.Shapes
+}
+
+data "oci_core_shapes" "tf_shapes" {
+  compartment_id      = var.compartment_id
+  image_id            = data.oci_core_images.tf_images.images.0.id
+}
+
