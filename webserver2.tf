3e310bb41a7a5d537e37c17ddcc29ea1108bd3e5
John Adewumi <johnadewumi63@gmail.com>
2021-06-03 14:40:23 -0700

    new

--- webserver2.tf
+++ webserver2.tf
@@ -2,7 +2,7 @@ resource "oci_core_instance" "johnserver2" {
 
   compartment_id      = var.compartment_id
 #   availability_domain = data.template_file.ad_names.*.rendered[0]
-  availability_domain = lookup(data.oci_identity_availability_domains.ad.availability_domain[0], "name")
+  availability_domain = lookup(data.oci_identity_availability_domains.ad.availability_domains[0], "name")
   shape               = var.Shapes
   display_name        = "johnserver2"
   subnet_id           = oci_core_subnet.publicsubnet.id
@@ -10,7 +10,7 @@ resource "oci_core_instance" "johnserver2" {
 
   source_details {
     source_type = "image"
-    source_id   = lookup(data.oci_core_images.oraclelinux-7-7.Images[0],"id")
+    source_id   = lookup(data.oci_core_images.oraclelinux-7-7.images[0],"id")
   }
   metadata = {
     ssh_authorized_keys = file(var.public_key_oci)
@@ -24,7 +24,7 @@ resource "oci_core_instance" "johnserver2" {
 data "oci_core_vnic_attachments" "john_vnic_attach2" {
   compartment_id      = var.compartment_id
 #   availability_domain = data.template_file.ad_names.*.rendered[0]
-  availability_domain = lookup(data.oci_identity_availability_domains.ad.availability_domain[0], "name")
+  availability_domain = lookup(data.oci_identity_availability_domains.ad.availability_domains[0], "name")
   instance_id         = oci_core_instance.johnserver2.id
 }
 
